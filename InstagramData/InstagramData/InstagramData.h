056b9f47b94166b73aef810dd05e36b88eb6f173
Ben Davis <ben@flypay.co.uk>
2016-12-26 18:52:06 +0000

    Added xcode projects

---
+++ InstagramData/InstagramData/InstagramData.h
@@ -0,0 +1,19 @@
+//
+//  InstagramData.h
+//  InstagramData
+//
+//  Created by Ben Davis on 26/12/2016.
+//  Copyright © 2016 bendavisapps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for InstagramData.
+FOUNDATION_EXPORT double InstagramDataVersionNumber;
+
+//! Project version string for InstagramData.
+FOUNDATION_EXPORT const unsigned char InstagramDataVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <InstagramData/PublicHeader.h>
+
+
